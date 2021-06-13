9f176fb023ba2ed49287d297a565ff77cc922051
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-06-13 22:32:16 +0200

    FIX: ]update template prefix name

--- infra/badges/email_templates.tf
+++ infra/badges/email_templates.tf
@@ -1,5 +1,5 @@
 resource "aws_ses_template" "badge_application_status_changed_template" {
-  name    = "${var.template_prefix}-badge-application-status-changed-template"
+  name    = "${local.template_prefix}-badge-application-status-changed-template"
   subject = "Your {{badgeName}} mApplication was updated to {{status}}"
   html    = file("${path.root}/emailTemplates/compiled/badge_application_status_changed.html")
   text    = file("${path.root}/emailTemplates/compiled/badge_application_status_changed.md")
