52406500da1be5a9a1f2cb32b3880fc59362fa64
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-06-13 22:34:40 +0200

    FIX: ]update template prefix name

--- infra/registry/email_templates.tf
+++ infra/registry/email_templates.tf
@@ -1,19 +1,19 @@
 resource "aws_ses_template" "organization_approval_requested_template" {
-  name    = "${local.template_prefix}-organization_approval-requested-template"
+  name    = "${local.template_prefix}-organization_approval-requested"
   subject = "{{displayName}} applied to the PublicSpaces Registry"
   html    = file("${path.root}/emailTemplates/compiled/organization_approval_requested.html")
   text    = file("${path.root}/emailTemplates/compiled/organization_approval_requested.md")
 }
 
 resource "aws_ses_template" "pending_organization_registration_template" {
-  name    = "${local.template_prefix}-pending-organization-template"
+  name    = "${local.template_prefix}-pending-organization"
   subject = "Your Application for {{displayName}} for the PublicSpaces Registry is Under Consideration"
   html    = file("${path.root}/emailTemplates/compiled/pending_organization_registration.html")
   text    = file("${path.root}/emailTemplates/compiled/pending_organization_registration.md")
 }
 
 resource "aws_ses_template" "approved_organization_registration_template" {
-  name    = "${local.template_prefix}-approved-organization-template"
+  name    = "${local.template_prefix}-approved-organization"
   subject = "{{displayName}} was accepted to the PublicSpaces Registry"
   html    = file("${path.root}/emailTemplates/compiled/approved_organization_registration.html")
   text    = file("${path.root}/emailTemplates/compiled/approved_organization_registration.md")
